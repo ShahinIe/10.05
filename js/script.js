739c236cf5b31e79b50f2191424d98e10adfd35c
Shahin I. Eyvazzada <ShahinIe@CODE.LOCAL>
2019-05-10 13:06:37 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// //  bPrice == 0 && sPrice == 0 ? true : (c.Price <= bPrice && c.Price >= sPrice)
+
+
+// // if (bPrice == 0, sPrice == 0) {
+// //     return (c.Price <= bPrice)
+
+// // } else {
+// //     return (c.Price >= sPrice)
+
+// // };
+
+
+
+
+// // // var reqem = 5;
+
+// // // reqem == 5 ? "beraberdir" : reqem > 5 ? "boyukdur" : "kicikdir";
+
+// // // if (reqem == 5) {
+// // //     return (beraberdir)
+
+  
+// // // }  else if(reqen>5) {
+// // //      return (boyukdur)
+// // // } else{
+// // //     return (kicikdir)
+// // // } ;
+
+
+// // var n=125
+
+// C
+// int a = 3;
+// int b = 5;
+
+// a = a + b;
+// b = b - a;
+// b = -b;
+// a = a - b;
+
+
+// var a = 3;
+// var b = 5;
+ 
+// a = a + b;
+// b = b - a;
+// b = -b;
+// a = a - b;
+// console.log(a) 
+// console.log(b) 
+
+
+
+
+var a = 36;
+var b = 54;
+ 
+a = a + b;
+b = b - a;
+b = -b;
+a = a - b;
+console.log(a) 
+console.log(b) 
+
+var n = 74, reverse = 0, remainder;
+
+while (n>0) {
+    remainder = n%10;
+    reverse = reverse * 10 + remainder;
+    n = Math.floor(n / 10);
+}
+
+
+
+console.log(reverse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
